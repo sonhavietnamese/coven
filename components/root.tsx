409e19bc358a71d6e17aff7cb6171ca1338a4b88
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-08-16 14:58:30 +0700

    feat: mock

---
+++ components/root.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { type PropsWithChildren, useEffect, useMemo, useState } from 'react'
+import {
+  SDKProvider,
+  useLaunchParams,
+  useMiniApp,
+  useThemeParams,
+  useViewport,
+  bindMiniAppCSSVars,
+  bindThemeParamsCSSVars,
+  bindViewportCSSVars,
+} from '@telegram-apps/sdk-react'
+import { AppRoot } from '@telegram-apps/telegram-ui'
+
+import { ErrorBoundary } from '@/components/error-boundary'
+import { ErrorPage } from '@/components/error-page'
+import { useTelegramMock } from '@/hooks/use-telegram-mock'
+import { useDidMount } from '@/hooks/use-did-mount'
+
+function App(props: PropsWithChildren) {
+  const lp = useLaunchParams()
+  const miniApp = useMiniApp()
+  const themeParams = useThemeParams()
+  const viewport = useViewport()
+
+  useEffect(() => {
+    return bindMiniAppCSSVars(miniApp, themeParams)
+  }, [miniApp, themeParams])
+
+  useEffect(() => {
+    return bindThemeParamsCSSVars(themeParams)
+  }, [themeParams])
+
+  useEffect(() => {
+    return viewport && bindViewportCSSVars(viewport)
+  }, [viewport])
+
+  return (
+    <AppRoot appearance={miniApp.isDark ? 'dark' : 'light'} platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}>
+      {props.children}
+    </AppRoot>
+  )
+}
+
+function RootInner({ children }: PropsWithChildren) {
+  // Mock Telegram environment in development mode if needed.
+  if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTelegramMock()
+  }
+
+  const debug = useLaunchParams().startParam === 'debug'
+  const manifestUrl = useMemo(() => {
+    return new URL('tonconnect-manifest.json', window.location.href).toString()
+  }, [])
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    if (debug) {
+      import('eruda').then((lib) => lib.default.init())
+    }
+  }, [debug])
+
+  return (
+    <SDKProvider acceptCustomStyles debug={debug}>
+      <App>{children}</App>
+    </SDKProvider>
+  )
+}
+
+export function Root(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of the Server Side
+  // Rendering. That's why we are showing loader on the server side.
+  const didMount = useDidMount()
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props} />
+    </ErrorBoundary>
+  ) : (
+    <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>Loading</div>
+  )
+}
